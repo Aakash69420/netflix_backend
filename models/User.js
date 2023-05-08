b86fd23cdfa6fefd41038158fb338b30da0d67a5
Aakash69420 <aakashbhattacharjee73@gmail.com>
2023-05-08 11:44:21 +0530

    localhost issues

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    contact: { type: String, default: "" },
+    contact: { type: String, required: true, default: "" },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     // contact: {
