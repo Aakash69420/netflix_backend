1d13810b182147de65fffafa2c418f3dc97b6f61
Aakash69420 <aakashbhattacharjee73@gmail.com>
2023-05-08 10:44:44 +0530

    localhost issues

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cors = require("./cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
@@ -28,7 +27,6 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
-app.use(cors);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
